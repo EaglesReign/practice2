d1bf4b3e652c1585ff64d76db704ac127fe804f0
Phillip <phv9495@saintjoe.edu>
2015-02-13 12:52:34 -0500

    Add the code for practice 2

---
+++ practice2.js
@@ -0,0 +1,14 @@
+//Phil Hofer
+//practice2.js
+
+var str = "My dog has teensy big fleas";
+
+function printChars(x,y,z) {
+  print(str.charAt(x));
+  print(str.charAt(y));
+  print(str.charAt(z));
+}
+
+printChars(4,7,10);
+print(str.toUpperCase());
+print(str.toLowerCase());
